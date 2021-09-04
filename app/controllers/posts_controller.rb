1551512d47db315a02af391977bdcae330606ae9
Colin Tseng <klt.aus@yahoo.com>
2021-09-04 18:44:40 +1000

    Completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,20 +24,20 @@ class PostsController < ApplicationController
 		redirect_to post_path(@post)
 	  end
 	  
-	  private
-	  
-	  
-	  # We pass the permitted fields in as *args;
-	  # this keeps `post_params` pretty dry while
-	  # still allowing slightly different behavior
-	  # depending on the controller action. This
-	  # should come after the other methods
-	  
-	  def post_params(*args)
-		params.require(:post).permit(*args)
-	  end
-	  
 	def edit
 	  @post = Post.find(params[:id])
+	end
+		  
+	private
+	  
+	  
+	# We pass the permitted fields in as *args;
+	# this keeps `post_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action. This
+	# should come after the other methods
+	
+	def post_params(*args)
+	  params.require(:post).permit(*args)
 	end
 end
